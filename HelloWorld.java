5ee88f33f72f3f2b517ead6416f5f4af714e9cef
upasana1808 <56124444+upasana1808@users.noreply.github.com>
2022-08-24 14:46:42 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("How are you");
+		System.out.println("I am good");
+		System.out.println("Long time no see");
 
 	}
 
